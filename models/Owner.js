d30a8ec77e18300bea17950118d07c0790c8d955
sanja avi <avi.sanja@gmail.com>
2020-08-06 12:40:43 +0700

    first commit

---
+++ models/Owner.js
@@ -0,0 +1,17 @@
+var mongoose = require('mongoose');
+var moment = require('moment');
+const Owner=mongoose.model("Owner",
+new mongoose.Schema({  
+    name: String,
+    user:{
+        type: String,
+        enum : ['Organization','Manager', 'Member'],
+        default: 'Member'
+    },
+    created: {type: Date, default: Date.now},
+    updated: {type: Date, default: Date.now},
+    project:String
+}));
+
+//moment().format(); 
+module.exports = Owner; 
